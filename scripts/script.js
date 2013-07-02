b9a52457392c3ae677f890a463fa654c172ff31a
raginikajale <ragini@localhost.localdomain>
2013-07-02 17:49:48 +0530

    add script file

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function GetinCtrl($scope) {
+		$scope.hidden = true;
+    	$scope.ragini=true;
+    	$scope.ragini1=true;
+    	$scope.ragini2=true;
+    	$scope.hidden1=true;
+    	$scope.fake=true;
+    
+    $scope.logger = function(vars){console.log("Value - " + vars);}
+}
